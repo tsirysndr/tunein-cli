366069df6f4a803324c42ffd5aeb69bedc91849d
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2025-08-27 20:48:54 +0300

    ci: link with libsystemd

--- .fluentci/src/dagger/lib.ts
+++ .fluentci/src/dagger/lib.ts
@@ -27,16 +27,16 @@ export function buildRustFlags(): string {
   let rustflags = "";
   switch (Deno.env.get("TARGET")) {
     case "aarch64-unknown-linux-gnu":
-      rustflags = `-C linker=aarch64-linux-gnu-gcc \
+      rustflags = `-Clink-arg=-lsystemd -C linker=aarch64-linux-gnu-gcc \
         -L/usr/aarch64-linux-gnu/lib \
         -L/build/sysroot/usr/lib/aarch64-linux-gnu \
-        -L/build/sysroot/lib/aarch64-linux-gnu -lsystemd`;
+        -L/build/sysroot/lib/aarch64-linux-gnu`;
       break;
     case "armv7-unknown-linux-gnueabihf":
-      rustflags = `-C linker=arm-linux-gnueabihf-gcc \
+      rustflags = `-Clink-arg=-lsystemd -C linker=arm-linux-gnueabihf-gcc \
         -L/usr/arm-linux-gnueabihf/lib \
         -L/build/sysroot/usr/lib/arm-linux-gnueabihf \
-        -L/build/sysroot/lib/arm-linux-gnueabihf -lsystemd`;
+        -L/build/sysroot/lib/arm-linux-gnueabihf`;
       break;
     default:
       break;
